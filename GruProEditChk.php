0450e3c9bb05f4f3b4b6f4ee40bde3f3aba94f92
Zayed Bin Maleque (News) <ne.zayed@gmail.com>
2017-01-03 00:12:17 +0600

    Add files via upload

---
+++ GruProEditChk.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+$signal="";
+$un="";
+
+$signal=$_SESSION["flag"];
+//$un=$_SESSION["fla"];
+
+
+
+if($signal=="oka"){
+	
+	$gID=$_POST['gid'];
+	$gName=$_POST['gname'];
+	$gEmail=$_POST['gEmail'];
+	$gPass=$_POST['pass'];
+
+	
+	
+	$conn = mysqli_connect("localhost", "root", "","sms");
+	if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//$sql = "INSERT INTO guardian (name, email, pass)
+//VALUES ('".$gName."', '".$gEmail."', '".$gPass."')";
+$sql="Delete from guardian WHERE id='".$gID."'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "<p style="."text-align:center;"." >Record Deleted successfully</p>";
+	echo "<br>";
+	echo "<p style="."text-align:center;"." ><a href="."GurProEdit.php".">Delete Another</a></p>";
+	echo "<br>";
+	echo "<p style="."text-align:center;"." ><a href="."admin.php".">Back To Admin Panel</a></p>";
+	echo "<br>";
+	echo "<p style="."text-align:center;"." ><a href="."logOut.php".">Log Out</a></p>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+}
+else{
+	header("Location:logOut.php");
+}
+?>
